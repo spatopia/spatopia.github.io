ec45fe2242d08fa623e42f286a988b53998e6e70
spatopia <spatopia@gmail.com>
2018-01-02 18:22:20 -0800

    Removed hammer slide functionality for now

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -16,46 +16,46 @@ $(() => {
         return false;
     });
 
-    const hammer = new Hammer($slide.get(0));
-    hammer.on('panleft', event => {
-        $slide.css('transform', `translateX(${event.deltaX}px)`);
-        $slide.css({
-            transform: `translateX(${event.deltaX}px)`,
-            transition: '0.2s easeInQuint'
-        });
-        deltaX = event.deltaX;
-    });
+    // const hammer = new Hammer($slide.get(0));
+    // hammer.on('panleft', event => {
+    //     $slide.css('transform', `translateX(${event.deltaX}px)`);
+    //     $slide.css({
+    //         transform: `translateX(${event.deltaX}px)`,
+    //         transition: '0.2s easeInQuint'
+    //     });
+    //     deltaX = event.deltaX;
+    // });
 
-    hammer.on('panright', event => {
-        if (deltaX <= 0) {
-            deltaX = Math.min(0, event.deltaX);
-            $slide.css({
-                transform: `translateX(${deltaX}px)`,
-                transition: '0s easeInQuint'
-            });
-        }
-    });
+    // hammer.on('panright', event => {
+    //     if (deltaX <= 0) {
+    //         deltaX = Math.min(0, event.deltaX);
+    //         $slide.css({
+    //             transform: `translateX(${deltaX}px)`,
+    //             transition: '0s easeInQuint'
+    //         });
+    //     }
+    // });
 
-    hammer.on('panend', event => {
-        if (-deltaX > $(document).width() / 2) {
-            $slide.css({
-                transform: `translateX(-105%)`,
-                transition: '0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940)'
-            });
-            $slide.removeClass('is-showing');
-            setTimeout(() => {
-                $review.addClass('is-hidden');
-            }, 500);
-            $('body').css('overflow', 'auto');
-        }
-        else {
-            $slide.css({
-                transform: `translateX(0)`,
-                transition: '0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940)'
-            });
-            deltaX = 0;
-        }
-    });
+    // hammer.on('panend', event => {
+    //     if (-deltaX > $(document).width() / 2) {
+    //         $slide.css({
+    //             transform: `translateX(-105%)`,
+    //             transition: '0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940)'
+    //         });
+    //         $slide.removeClass('is-showing');
+    //         setTimeout(() => {
+    //             $review.addClass('is-hidden');
+    //         }, 500);
+    //         $('body').css('overflow', 'auto');
+    //     }
+    //     else {
+    //         $slide.css({
+    //             transform: `translateX(0)`,
+    //             transition: '0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940)'
+    //         });
+    //         deltaX = 0;
+    //     }
+    // });
 
     $('.js-spa-review__back').on('click touch', event => {
             $slide.css({
